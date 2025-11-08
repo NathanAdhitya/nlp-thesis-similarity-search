f4168fab4840d041c135cb81b6293b3281aebb50
Nathan Adhitya <NathanAdhitya@users.noreply.github.com>
2025-11-08 20:04:04 +0700

    test all models in locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,15 +1,39 @@
 import time
-from locust import HttpUser, task, between
+from locust import HttpUser, User, task, between, tag, TaskSet
 
-class DefaultUser(HttpUser):
-    search_query = "Computer Networking"
-    model = "bgem3"
-    top_k = 100
 
+
+
+def create_task(model: str):
+    search_query: str = "Computer Networking"
+    top_k: int = 100
+    
+    @tag(model, "author_search")
     @task
     def author_search(self):
-        self.client.get(f"/search/author/{self.search_query}?topK={self.top_k}&model={self.model}")
+        self.client.get(
+            f"/search/author/{search_query}?topK={top_k}&model={model}"
+        )
 
+    @tag(model, "paper_search")
     @task
     def paper_search(self):
-        self.client.get(f"/search/paper/{self.search_query}?topK={self.top_k}&model={self.model}")
+        self.client.get(
+            f"/search/paper/{search_query}?topK={top_k}&model={model}"
+        )
+        
+    return author_search, paper_search
+
+class DefaultUser(HttpUser):
+    models = [
+        "bgem3",
+        "allminilm",
+        "indobert"
+    ]
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for model in self.models:
+            tasks = create_task(model)
+            for task in tasks:
+                self.tasks.append(task)
